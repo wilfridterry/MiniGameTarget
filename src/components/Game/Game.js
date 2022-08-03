cba0058528c79005ee2a2673c436affa5718e76c
Stannis <zalevschi1998@gmail.com>
2022-08-04 00:01:21 +0300

    Started to add a structure

---
+++ src/components/Game/Game.js
@@ -0,0 +1,16 @@
+import "./Game.css";
+
+const Game = () => {
+  return (
+    <section className="poligon">
+        <div className="panel">
+            Tries: 5;  
+        </div>
+        <div className="game-field"> 
+
+        </div>
+    </section>
+  );
+};
+
+export default Game;
